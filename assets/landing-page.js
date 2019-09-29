6b542de04fbcb17f695307498f9999ba435f2fae
Altana Wealth Prototype <altana@positivemojo.33mail.com>
2019-09-29 19:52:39 +0000

    Delete: assets/landing-page.js

--- assets/landing-page.js
+++
@@ -1,4 +0,0 @@
-window.addEventListener('DOMContentLoaded', function() {
-  document.getElementsByTagName("BODY")[0].classList.add("landing-page");
-  document.getElementsByClassName("Parallax-host-outer")[0].getElementsByClassName("Parallax-item")[0].getElementsByTagName("IMG")[0].classList.add("back-image");
-});

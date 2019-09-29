aba4acb6c0d17f201ea79e5b4b6e251fa5ea3cbf
Altana Wealth Prototype <altana@positivemojo.33mail.com>
2019-09-29 18:43:13 +0000

    Upload: assets/landing-page.js

--- assets/landing-page.js
+++ assets/landing-page.js
@@ -1,4 +1,4 @@
-var bodyID;
 window.addEventListener('DOMContentLoaded', function() {
   document.getElementsByTagName("BODY")[0].classList.add("landing-page");
+  document.getElementsByClassName("Parallax-host-outer")[0].getElementsByClassName("Parallax-item")[0].getElementsByTagName("IMG")[0].add("back-image");
 });

b9e0a92e2578883e318ff89cc2009df40b329493
Altana Wealth Prototype <altana@positivemojo.33mail.com>
2019-09-30 11:02:11 +0000

    Upload: assets/fund-styling.js

--- assets/fund-styling.js
+++ assets/fund-styling.js
@@ -10,9 +10,9 @@ function addFundClassIfRequired() {
     var thisPage = window.location.pathname.split('/').slice(-1)[0];
 
     // test if page exists in pageToClassMap array
-    var result = pageToClassMap.filter(x => x.pageName === thisPage)[0];
+    var result = pageToClassMap.filter(x => x.pageName === thisPage)[0].fundClass;
 
-    if result.fundClass.length != 0 {
+    if (!result || result.length != 0) {
       document.getElementsByTagName("BODY")[0].classList.add(result.fundClass);
     } else {
     }

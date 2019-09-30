5bc79d213aa15cb1c5a217cbf09f1dd1ca84c064
Altana Wealth Prototype <altana@positivemojo.33mail.com>
2019-09-30 06:01:23 +0000

    Upload: assets/header-sticky.js

---
+++ assets/header-sticky.js
@@ -0,0 +1,16 @@
+window.addEventListener('DOMContentLoaded', function() {
+  window.addEventListener("scroll", function() {toggleStickyHeader()});
+});
+  // When the user scrolls the page, execute toggleStickyHeader 
+
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function toggleStickyHeader() {
+  // Get the header
+  var headerID = document.getElementsByTagName("header");
+  if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+    headerID[0].classList.add("sticky");
+  } else {
+    headerID[0].classList.remove("sticky");
+  }  
+}

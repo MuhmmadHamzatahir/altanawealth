f8f5bd7edd15f8a88b02f4bcb0615efb4f55c948
Altana Wealth Prototype <altana@positivemojo.33mail.com>
2019-10-09 19:46:00 +0000

    Upload: assets/fund-styling.js

--- assets/fund-styling.js
+++ assets/fund-styling.js
@@ -1,13 +1,33 @@
-/*
 var pageToClassMap = [
 {pageName: 'adas-overview', fundClass: 'adas'},
 {pageName: 'adas-fund-information', fundClass: 'adas'},
+{pageName: 'adas-research-and-sentiment-data', fundClass: 'adas'},
+{pageName: 'adas-directors-report-us', fundClass: 'adas'},
+{pageName: 'adas-directors-report-europe', fundClass: 'adas'},
+{pageName: 'adas-directors-report-asia', fundClass: 'adas'},
 {pageName: 'acbf-overview', fundClass: 'acbf'},
 {pageName: 'acbf-fund-information', fundClass: 'acbf'},
-
+{pageName: 'apas-strategy-information', fundClass: 'apas'},
+{pageName: 'tbf-overview', fundClass: 'tbf'},
+{pageName: 'tbf-fund-information', fundClass: 'tbf'},
+{pageName: 'pssif-overview', fundClass: 'pssif'},
+{pageName: 'pssif-fund-information', fundClass: 'pssif'},
+{pageName: 'aspa-overview', fundClass: 'aspa'},
+{pageName: 'aspa-strategy-information', fundClass: 'aspa'},
+{pageName: 'acf-overview', fundClass: 'acf'},
+{pageName: 'acf-fund-information', fundClass: 'acf'},
+{pageName: 'actf-overview', fundClass: 'actf'},
+{pageName: 'actf-strategy-information', fundClass: 'actf'},
+{pageName: 'adcf-overview', fundClass: 'adcf'},
+{pageName: 'adcf-strategy-information', fundClass: 'adcf'},
+{pageName: 'adof-overview', fundClass: 'adof'},
+{pageName: 'adof-strategy-information', fundClass: 'adof'},
+{pageName: 'all-funds-documents', fundClass: ''},
 {pageName: 'all-funds', fundClass: ''}
 ];
-*/
+
+
+
 function addFundClassIfRequired() {
   // get name of page
   var thisPage = window.location.pathname.split('/').slice(-1)[0];

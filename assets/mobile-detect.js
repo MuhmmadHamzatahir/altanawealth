1aa8d71f14d6a9208302e938a0dc5895305b0854
Altana Wealth Prototype <altana@positivemojo.33mail.com>
2019-10-16 19:32:10 +0000

    Upload: assets/mobile-detect.js

---
+++ assets/mobile-detect.js
@@ -0,0 +1,18 @@
+var usingOnMobile = detectmob();
+
+function detectmob() {
+    if (navigator.userAgent.match(/Android/i) ||
+        navigator.userAgent.match(/webOS/i) ||
+        navigator.userAgent.match(/iPhone/i) ||
+        navigator.userAgent.match(/iPad/i) ||
+        navigator.userAgent.match(/iPod/i) ||
+        navigator.userAgent.match(/BlackBerry/i) ||
+        navigator.userAgent.match(/Windows Phone/i)
+    ) {
+        console.log('running on mobile <' + navigator.userAgent + '>');
+        return true;
+    } else {
+        console.log('running on non-mobile <' + navigator.userAgent + '>');
+        return false;
+    }
+}

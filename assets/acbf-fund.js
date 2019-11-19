7f9e5e870a1c28f0408cf941ef7a9fb979098371
Altana Wealth Prototype <altana@positivemojo.33mail.com>
2019-11-19 12:19:41 +0000

    Upload: assets/acbf-fund.js

--- assets/acbf-fund.js
+++ assets/acbf-fund.js
@@ -5,3 +5,17 @@ function drawCharts (){
     loadVisualization('acbf_monthly_perf_ALTCBAE.csv', drawPerfTable, 'acbf-fund-table03');
     loadVisualization('ACBF_share_classes.csv', drawPerfTable, 'acbf-fund-table04');
 }
+function drawGoogleVisualizations() {
+    // Load the Visualization API and the corechart package.
+    google.charts.load('current', {
+        'packages': ['corechart', 'table', 'gauge', 'geochart']
+    });
+    // Set a callback to run when the Google Visualization API is loaded.
+    google.charts.setOnLoadCallback(drawCharts);
+}
+window.addEventListener('DOMContentLoaded', drawGoogleVisualizations);
+window.onresize = function(event) {
+    drawGoogleVisualizations();
+};
+
+

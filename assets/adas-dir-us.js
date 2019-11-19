acd21d287b3f626817ede726c7a8a23aeda817fe
Altana Wealth Prototype <altana@positivemojo.33mail.com>
2019-11-19 17:51:19 +0000

    Upload: assets/adas-dir-us.js

--- assets/adas-dir-us.js
+++ assets/adas-dir-us.js
@@ -1,10 +1,10 @@
 function drawCharts(){
-    loadVisualization('existing/map_us.csv', drawMap, 'adas-dir-us-map01');
+    loadVisualization('existing/map_us.csv', drawMap, 'adas-dir-us-map01', 'US', 'provinces');
     loadVisualization('existing/director_activity_ratio.csv', drawGauge, 'adas-dir-us-gauge01');
     loadVisualization('existing/director_activity.csv', drawArea, 'adas-dir-us-area01');
-    loadVisualization('existing/growth_aias.csv', drawLine, 'adas-dir-us-line01');
-    loadVisualization('existing/gross_net_exposure.csv', drawArea, 'adas-dir-us-area02');
-    loadVisualization('existing/gross_net_exposure.csv', drawArea, 'adas-dir-us-area03');
+    loadVisualization('existing/director_activity_ratio_history.csv', drawLine, 'adas-dir-us-line01');
+    loadVisualization('existing/filings_transactions.csv', drawArea, 'adas-dir-us-area02');
+    loadVisualization('existing/director_rank_pecent.csv', drawArea, 'adas-dir-us-area03', 'percent');
 }
 function drawGoogleVisualizations() {
     // Load the Visualization API and the corechart package.

cf85e40ca19bddd5e9b2fafe96e829e539ffc512
Altana Wealth Prototype <altana@positivemojo.33mail.com>
2019-11-19 16:35:22 +0000

    Upload: assets/adas-dir.js

--- assets/adas-dir.js
+++ assets/adas-dir.js
@@ -1,5 +1,5 @@
 function drawCharts() {
-    loadVisualization('existing/global_map.csv', drawMap, 'adas-dir-map02');
+    loadVisualization('existing/global_map.csv', drawMap, 'adas-dir-map01');
     loadVisualization('existing/director_activity_ratio.csv', drawGauge, 'adas-dir-gauge01');
     loadVisualization('existing/eu_director_activity_ratio.csv', drawGauge, 'adas-dir-gauge02');
     loadVisualization('existing/au_director_activity_ratio.csv', drawGauge, 'adas-dir-gauge03');

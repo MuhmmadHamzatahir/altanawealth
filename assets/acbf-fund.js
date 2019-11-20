33cde72776d0f1496383edb39834a60d3c0e92d1
Altana Wealth Prototype <altana@positivemojo.33mail.com>
2019-11-20 16:28:45 +0000

    Upload: assets/acbf-fund.js

--- assets/acbf-fund.js
+++ assets/acbf-fund.js
@@ -1,8 +1,8 @@
 function drawCharts (){
     loadVisualization('acbf_monthly_perf_ALTCBOU.csv', drawSummaryFromPerfData, 'acbf-fund-table05');
-    loadVisualization('acbf_monthly_perf_ALTCBOU.csv', drawPerfTable, 'acbf-fund-table01');
-    loadVisualization('acbf_monthly_perf_ALTCBCG.csv', drawPerfTable, 'acbf-fund-table02');
-    loadVisualization('acbf_monthly_perf_ALTCBAE.csv', drawPerfTable, 'acbf-fund-table03');
+    loadVisualization('acbf_monthly_perf_ALTCBOU.csv', drawPerfTable, 'acbf-fund-table01', false, 2);
+    loadVisualization('acbf_monthly_perf_ALTCBCG.csv', drawPerfTable, 'acbf-fund-table02', false, 2);
+    loadVisualization('acbf_monthly_perf_ALTCBAE.csv', drawPerfTable, 'acbf-fund-table03', false, 2);
     loadVisualization('ACBF_share_classes.csv', drawPerfTable, 'acbf-fund-table04');
 }
 function drawGoogleVisualizations() {

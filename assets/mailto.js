04ff32cd6b91a43ae01ea8cc44749ff89d275542
Altana Wealth Prototype <altana@positivemojo.33mail.com>
2019-11-21 19:06:06 +0000

    Upload: assets/mailto.js

---
+++ assets/mailto.js
@@ -0,0 +1,9 @@
+window.addEventListener('DOMContentLoaded', function() {
+
+    var anchors = document.getElementsByTagName("a");
+
+    for (var i = anchors.length - 1; i >= 0; i--) {
+      if (anchors[i].href.includes('mailto'))
+        anchors[i].classList.add('mailto');
+    }
+});

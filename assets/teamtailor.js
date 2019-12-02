649d083111665a568cb89a8c0d69f0cc5c6b3566
Altana Wealth Prototype <altana@positivemojo.33mail.com>
2019-12-02 12:47:32 +0000

    Upload: assets/teamtailor.js

---
+++ assets/teamtailor.js
@@ -0,0 +1,24 @@
+(function(altana, undefined) {
+  altana.teamtailorSettings = {
+      widgetPosition: 'bottom-right',
+      color: '#b89d18',
+      company: 'altanawealth.teamtailor.com'
+  }
+
+  var w = window;
+  var d = document;
+
+  function l() {
+      var s = d.createElement('script');
+      s.type = 'text/javascript';
+      s.async = true;
+      s.src = '//' + altana.teamtailorSettings.company + '/widget';
+      var x = d.getElementsByTagName('script')[0];
+      x.parentNode.insertBefore(s, x);
+  }
+  if (w.attachEvent) {
+      w.attachEvent('onload', l);
+  } else {
+      w.addEventListener('load', l, false);
+  }
+})(window.altana = window.altana || {});

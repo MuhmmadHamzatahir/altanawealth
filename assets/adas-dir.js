35384b4e327ad3c408259f834a15e309fe10662b
Altana Wealth Prototype <altana@positivemojo.33mail.com>
2019-12-06 11:43:44 +0000

    Upload: assets/adas-dir.js

--- assets/adas-dir.js
+++ assets/adas-dir.js
@@ -1,12 +1,10 @@
 (function(altana, undefined) {
-
     function drawCharts() {
     altana.loadVisualization('existing/global_map.csv', altana.drawMap, 'adas-dir-map01');
     altana.loadVisualization('existing/director_activity_ratio.csv', altana.drawGauge, 'adas-dir-gauge01');
     altana.loadVisualization('existing/eu_director_activity_ratio.csv', altana.drawGauge, 'adas-dir-gauge02');
     altana.loadVisualization('existing/au_director_activity_ratio.csv', altana.drawGauge, 'adas-dir-gauge03');
     }
-
     function drawGoogleVisualizations() {
         // Load the Visualization API and the corechart package.
         google.charts.load('current', {
@@ -15,11 +13,8 @@
         // Set a callback to run when the Google Visualization API is loaded.
         google.charts.setOnLoadCallback(drawCharts);
     }
-
-
     window.addEventListener('DOMContentLoaded', drawGoogleVisualizations);
     window.onresize = function(event) {
         drawGoogleVisualizations();
     };
-
 })(window.altana = window.altana || {});

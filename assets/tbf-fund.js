7ebe981bec9d21c78d2330e5b701d4aadbd1adbe
Altana Wealth Prototype <altana@positivemojo.33mail.com>
2019-12-06 11:22:51 +0000

    Upload: assets/tbf-fund.js

--- assets/tbf-fund.js
+++ assets/tbf-fund.js
@@ -1,22 +1,27 @@
-function drawCharts (){
-    loadVisualization('TBF+Simulated+Perf+History.csv', drawLine, 'tbf-fund-line01');
-    loadVisualization('TBF+Summary+Statistical+Data.csv', drawPerfTable, 'tbf-fund-table01');
-    loadVisualization('TBF+Annual+Sim+Perf+Summary.csv', drawPerfTable, 'tbf-fund-table02', true, 1);
-    loadVisualization('TBF+Fund+Codes.csv', drawPerfTable, 'tbf-fund-table03');
-    loadVisualization('TBF+Sector+Alloc.csv', drawColumn, 'tbf-fund-column01');
-    loadVisualization('TBF+Sector+Alloc+full+titles.csv', drawPerfTable, 'tbf-fund-table04');
-}
-function drawGoogleVisualizations() {
-    // Load the Visualization API and the corechart package.
-    google.charts.load('current', {
-        'packages': ['corechart', 'table', 'gauge', 'geochart']
-    });
-    // Set a callback to run when the Google Visualization API is loaded.
-    google.charts.setOnLoadCallback(drawCharts);
-}
-window.addEventListener('DOMContentLoaded', drawGoogleVisualizations);
-window.onresize = function(event) {
-    drawGoogleVisualizations();
-};
+(function(altana, undefined) {
+
+    function drawCharts() {
+    altana.loadVisualization('TBF+Simulated+Perf+History.csv', altana.drawLine, 'tbf-fund-line01');
+    altana.loadVisualization('TBF+Summary+Statistical+Data.csv', altana.drawPerfTable, 'tbf-fund-table01');
+    altana.loadVisualization('TBF+Annual+Sim+Perf+Summary.csv', altana.drawPerfTable, 'tbf-fund-table02', true, 1);
+    altana.loadVisualization('TBF+Fund+Codes.csv', altana.drawPerfTable, 'tbf-fund-table03');
+    altana.loadVisualization('TBF+Sector+Alloc.csv', altana.drawColumn, 'tbf-fund-column01');
+    altana.loadVisualization('TBF+Sector+Alloc+full+titles.csv', altana.drawPerfTable, 'tbf-fund-table04');
+    }
+
+    function drawGoogleVisualizations() {
+        // Load the Visualization API and the corechart package.
+        google.charts.load('current', {
+            'packages': ['corechart', 'table', 'gauge', 'geochart']
+        });
+        // Set a callback to run when the Google Visualization API is loaded.
+        google.charts.setOnLoadCallback(drawCharts);
+    }
 
 
+    window.addEventListener('DOMContentLoaded', drawGoogleVisualizations);
+    window.onresize = function(event) {
+        drawGoogleVisualizations();
+    };
+
+})(window.altana = window.altana || {});

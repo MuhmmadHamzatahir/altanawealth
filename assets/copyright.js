a7fddefce61cff37b7464f6c5df4948bd3136dbe
Altana Wealth Prototype <altana@positivemojo.33mail.com>
2019-12-06 15:29:45 +0000

    Upload: assets/copyright.js

--- assets/copyright.js
+++ assets/copyright.js
@@ -1,3 +1,5 @@
-window.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('copyright-year').innerHTML= 1900 + (new Date()).getYear();
-});
+(function(altana, undefined) {
+    window.addEventListener('DOMContentLoaded', function() {
+        document.getElementById('copyright-year').innerHTML = 1900 + (new Date()).getYear();
+    });
+})(window.altana = window.altana || {});

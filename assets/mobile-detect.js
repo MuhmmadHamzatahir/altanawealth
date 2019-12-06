d5f91ba14a97bd4017e225443d13a6f61c3f37b5
Altana Wealth Prototype <altana@positivemojo.33mail.com>
2019-12-06 15:42:35 +0000

    Upload: assets/mobile-detect.js

--- assets/mobile-detect.js
+++ assets/mobile-detect.js
@@ -1,32 +1,37 @@
-var usingOnMobile = null;
+(function(altana, undefined) {
+    var usingOnMobile = null;
 
-window.addEventListener('DOMContentLoaded', function() {
-    window.addEventListener('resize', handleWindowResize);
-    handleWindowResize();
-});
-function handleWindowResize() {
-    usingOnMobile = detectmob();
-    var bodyID = document.getElementsByTagName("BODY")[0];
+    window.addEventListener('DOMContentLoaded', function() {
+        window.addEventListener('resize', handleWindowResize);
+        handleWindowResize();
+    });
 
-    if (usingOnMobile) {
-        bodyID.classList.add("altana-mobile");
-        bodyID.classList.remove("altana-not-mobile");
-    } else {
-        bodyID.classList.add("altana-not-mobile");
-        bodyID.classList.remove("altana-mobile");
+    function handleWindowResize() {
+        usingOnMobile = detectmob();
+        var bodyID = document.getElementsByTagName("BODY")[0];
+
+        if (usingOnMobile) {
+            bodyID.classList.add("altana-mobile");
+            bodyID.classList.remove("altana-not-mobile");
+        } else {
+            bodyID.classList.add("altana-not-mobile");
+            bodyID.classList.remove("altana-mobile");
+        }
+        console.log("<" + navigator.userAgent + "><" + usingOnMobile + ">");
+    };
+
+    function detectmob() {
+        if (navigator.userAgent.match(/Android/i) ||
+            navigator.userAgent.match(/webOS/i) ||
+            navigator.userAgent.match(/iPhone/i) ||
+            navigator.userAgent.match(/iPad/i) ||
+            navigator.userAgent.match(/iPod/i) ||
+            navigator.userAgent.match(/BlackBerry/i) ||
+            navigator.userAgent.match(/Windows Phone/i)
+        ) {
+            return true;
+        } else {
+            return false;
+        }
     }
-};
-function detectmob() {
-    if (navigator.userAgent.match(/Android/i) ||
-        navigator.userAgent.match(/webOS/i) ||
-        navigator.userAgent.match(/iPhone/i) ||
-        navigator.userAgent.match(/iPad/i) ||
-        navigator.userAgent.match(/iPod/i) ||
-        navigator.userAgent.match(/BlackBerry/i) ||
-        navigator.userAgent.match(/Windows Phone/i)
-    ) {
-        return true;
-    } else {
-        return false;
-    }
-}
+})(window.altana = window.altana || {});

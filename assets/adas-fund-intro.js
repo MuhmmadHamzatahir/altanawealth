c265f2eea619a50b8c27e79c5cf96af82c68d081
Altana Wealth Prototype <altana@positivemojo.33mail.com>
2019-12-06 11:12:05 +0000

    Upload: assets/adas-fund-intro.js

--- assets/adas-fund-intro.js
+++ assets/adas-fund-intro.js
@@ -1,15 +1,22 @@
-function drawCharts(){
-    loadVisualization('adas_monthly_perf_ALTDASA.csv', drawSummaryFromPerfData, 'adas-fund-intro-table01');
-}
-function drawGoogleVisualizations() {
-    // Load the Visualization API and the corechart package.
-    google.charts.load('current', {
-        'packages': ['corechart', 'table', 'gauge', 'geochart']
-    });
-    // Set a callback to run when the Google Visualization API is loaded.
-    google.charts.setOnLoadCallback(drawCharts);
-}
-window.addEventListener('DOMContentLoaded', drawGoogleVisualizations);
-window.onresize = function(event) {
-    drawGoogleVisualizations();
-};
+(function(altana, undefined) {
+
+    function drawCharts() {
+        altana.loadVisualization('adas_monthly_perf_ALTDASA.csv', altana.drawSummaryFromPerfData, 'adas-fund-intro-table01');
+    }
+
+    function drawGoogleVisualizations() {
+        // Load the Visualization API and the corechart package.
+        google.charts.load('current', {
+            'packages': ['corechart', 'table', 'gauge', 'geochart']
+        });
+        // Set a callback to run when the Google Visualization API is loaded.
+        google.charts.setOnLoadCallback(drawCharts);
+    }
+
+
+    window.addEventListener('DOMContentLoaded', drawGoogleVisualizations);
+    window.onresize = function(event) {
+        drawGoogleVisualizations();
+    };
+
+})(window.altana = window.altana || {});

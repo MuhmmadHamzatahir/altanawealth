286c6ff42a0e7f93b6b4bee680cd4007ffedd420
Altana Wealth Prototype <altana@positivemojo.33mail.com>
2019-12-06 15:39:02 +0000

    Upload: assets/header-sticky.js

--- assets/header-sticky.js
+++ assets/header-sticky.js
@@ -1,16 +1,20 @@
-window.addEventListener('DOMContentLoaded', function() {
-  window.addEventListener("scroll", function() {toggleStickyHeader()});
-});
-  // When the user scrolls the page, execute toggleStickyHeader 
+(function(altana, undefined) {
+    window.addEventListener('DOMContentLoaded', function() {
+        window.addEventListener("scroll", function() {
+            toggleStickyHeader()
+        });
+    });
+    // When the user scrolls the page, execute toggleStickyHeader 
 
 
-// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function toggleStickyHeader() {
-  // Get the header
-  var headerID = document.getElementsByTagName("header");
-  if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
-    headerID[0].classList.add("sticky");
-  } else {
-    headerID[0].classList.remove("sticky");
-  }  
-}
+    // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+    function toggleStickyHeader() {
+        // Get the header
+        var headerID = document.getElementsByTagName("header");
+        if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+            headerID[0].classList.add("sticky");
+        } else {
+            headerID[0].classList.remove("sticky");
+        }
+    }
+})(window.altana = window.altana || {});

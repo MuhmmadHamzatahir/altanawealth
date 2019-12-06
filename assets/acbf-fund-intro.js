23a4ba69cdcd2c76a8249848475ad9daf079c2a8
Altana Wealth Prototype <altana@positivemojo.33mail.com>
2019-12-06 11:22:52 +0000

    Upload: assets/acbf-fund-intro.js

--- assets/acbf-fund-intro.js
+++ assets/acbf-fund-intro.js
@@ -1,15 +1,22 @@
-function drawCharts(){
-    loadVisualization('acbf_monthly_perf_ALTCBOU.csv', drawSummaryFromPerfData, 'acbf-fund-intro-table01');
-}
-function drawGoogleVisualizations() {
-    // Load the Visualization API and the corechart package.
-    google.charts.load('current', {
-        'packages': ['corechart', 'table', 'gauge', 'geochart']
-    });
-    // Set a callback to run when the Google Visualization API is loaded.
-    google.charts.setOnLoadCallback(drawCharts);
-}
-window.addEventListener('DOMContentLoaded', drawGoogleVisualizations);
-window.onresize = function(event) {
-    drawGoogleVisualizations();
-};
+(function(altana, undefined) {
+
+    function drawCharts() {
+    altana.loadVisualization('acbf_monthly_perf_ALTCBOU.csv', altana.drawSummaryFromPerfData, 'acbf-fund-intro-table01');
+    }
+
+    function drawGoogleVisualizations() {
+        // Load the Visualization API and the corechart package.
+        google.charts.load('current', {
+            'packages': ['corechart', 'table', 'gauge', 'geochart']
+        });
+        // Set a callback to run when the Google Visualization API is loaded.
+        google.charts.setOnLoadCallback(drawCharts);
+    }
+
+
+    window.addEventListener('DOMContentLoaded', drawGoogleVisualizations);
+    window.onresize = function(event) {
+        drawGoogleVisualizations();
+    };
+
+})(window.altana = window.altana || {});

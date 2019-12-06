4ebeb61ff22e3749c782bf30a23c2bf3a8d4576d
Altana Wealth Prototype <altana@positivemojo.33mail.com>
2019-12-06 11:22:51 +0000

    Upload: assets/adas-dir-us.js

--- assets/adas-dir-us.js
+++ assets/adas-dir-us.js
@@ -1,20 +1,27 @@
-function drawCharts(){
-    loadVisualization('existing/map_us.csv', drawMap, 'adas-dir-us-map01', 'US', 'provinces');
-    loadVisualization('existing/director_activity_ratio.csv', drawGauge, 'adas-dir-us-gauge01');
-    loadVisualization('existing/director_activity.csv', drawArea, 'adas-dir-us-area01');
-    loadVisualization('existing/director_activity_ratio_history.csv', drawLine, 'adas-dir-us-line01');
-    loadVisualization('existing/filings_transactions.csv', drawArea, 'adas-dir-us-area02');
-    loadVisualization('existing/director_rank_pecent.csv', drawArea, 'adas-dir-us-area03', 'percent');
-}
-function drawGoogleVisualizations() {
-    // Load the Visualization API and the corechart package.
-    google.charts.load('current', {
-        'packages': ['corechart', 'table', 'gauge', 'geochart']
-    });
-    // Set a callback to run when the Google Visualization API is loaded.
-    google.charts.setOnLoadCallback(drawCharts);
-}
-window.addEventListener('DOMContentLoaded', drawGoogleVisualizations);
-window.onresize = function(event) {
-    drawGoogleVisualizations();
-};
+(function(altana, undefined) {
+
+    function drawCharts() {
+    altana.loadVisualization('existing/map_us.csv', altana.drawMap, 'adas-dir-us-map01', 'US', 'provinces');
+    altana.loadVisualization('existing/director_activity_ratio.csv', altana.drawGauge, 'adas-dir-us-gauge01');
+    altana.loadVisualization('existing/director_activity.csv', altana.drawArea, 'adas-dir-us-area01');
+    altana.loadVisualization('existing/director_activity_ratio_history.csv', altana.drawLine, 'adas-dir-us-line01');
+    altana.loadVisualization('existing/filings_transactions.csv', altana.drawArea, 'adas-dir-us-area02');
+    altana.loadVisualization('existing/director_rank_pecent.csv', altana.drawArea, 'adas-dir-us-area03', 'percent');
+    }
+
+    function drawGoogleVisualizations() {
+        // Load the Visualization API and the corechart package.
+        google.charts.load('current', {
+            'packages': ['corechart', 'table', 'gauge', 'geochart']
+        });
+        // Set a callback to run when the Google Visualization API is loaded.
+        google.charts.setOnLoadCallback(drawCharts);
+    }
+
+
+    window.addEventListener('DOMContentLoaded', drawGoogleVisualizations);
+    window.onresize = function(event) {
+        drawGoogleVisualizations();
+    };
+
+})(window.altana = window.altana || {});

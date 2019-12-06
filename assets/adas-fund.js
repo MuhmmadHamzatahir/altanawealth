1a4fbf2e449d9f06626f42b98ed2e89be820843c
Altana Wealth Prototype <altana@positivemojo.33mail.com>
2019-12-06 11:12:06 +0000

    Upload: assets/adas-fund.js

--- assets/adas-fund.js
+++ assets/adas-fund.js
@@ -1,18 +1,21 @@
+(function(altana, undefined){
+
 function drawCharts(){
-    loadVisualization('existing/growth_aias.csv', drawLine, 'adas-fund-line01');
-    loadVisualization('adas_monthly_perf_ALTDASA.csv', drawPerfTable, 'adas-fund-table01', false, 2);
-    loadVisualization('adas_monthly_perf_GS.csv', drawPerfTable, 'adas-fund-table02', false, 2);
-    loadVisualization('adas_monthly_perf_HFRX.csv', drawPerfTable, 'adas-fund-table03', false, 2);
-    loadVisualization('adas_monthly_perf_split_returns.csv', drawPerfTable, 'adas-fund-table04', false, 2);
-    loadVisualization('existing/gross_net_exposure.csv', drawArea, 'adas-fund-area01');
-    loadVisualization('existing/open_positions.csv', drawArea, 'adas-fund-area02');
-    loadVisualization('adas_monthly_perf_gross_net.csv', drawPerfTable, 'adas-fund-table05', false, 0);
-    loadVisualization('existing/sector_exposure_long.csv', drawDonut, 'adas-fund-donut01');
-    loadVisualization('existing/sector_exposure_short.csv', drawDonut, 'adas-fund-donut02');
-    loadVisualization('existing/cap_aias_net.csv', drawColumn, 'adas-fund-column01');
-    loadVisualization('ADAS_share_classes.csv', drawPerfTable, 'adas-fund-table06');
-    loadVisualization('adas_monthly_perf_ALTDASA.csv', drawSummaryFromPerfData, 'adas-fund-table07');
+    altana.loadVisualization('existing/growth_aias.csv', altana.drawLine, 'adas-fund-line01');
+    altana.loadVisualization('adas_monthly_perf_ALTDASA.csv', altana.drawPerfTable, 'adas-fund-table01', false, 2);
+    altana.loadVisualization('adas_monthly_perf_GS.csv', altana.drawPerfTable, 'adas-fund-table02', false, 2);
+    altana.loadVisualization('adas_monthly_perf_HFRX.csv', altana.drawPerfTable, 'adas-fund-table03', false, 2);
+    altana.loadVisualization('adas_monthly_perf_split_returns.csv', altana.drawPerfTable, 'adas-fund-table04', false, 2);
+    altana.loadVisualization('existing/gross_net_exposure.csv', altana.drawArea, 'adas-fund-area01');
+    altana.loadVisualization('existing/open_positions.csv', altana.drawArea, 'adas-fund-area02');
+    altana.loadVisualization('adas_monthly_perf_gross_net.csv', altana.drawPerfTable, 'adas-fund-table05', false, 0);
+    altana.loadVisualization('existing/sector_exposure_long.csv', altana.drawDonut, 'adas-fund-donut01');
+    altana.loadVisualization('existing/sector_exposure_short.csv', altana.drawDonut, 'adas-fund-donut02');
+    altana.loadVisualization('existing/cap_aias_net.csv', altana.drawColumn, 'adas-fund-column01');
+    altana.loadVisualization('ADAS_share_classes.csv', altana.drawPerfTable, 'adas-fund-table06');
+    altana.loadVisualization('adas_monthly_perf_ALTDASA.csv', altana.drawSummaryFromPerfData, 'adas-fund-table07');
 }
+
 function drawGoogleVisualizations() {
     // Load the Visualization API and the corechart package.
     google.charts.load('current', {
@@ -21,7 +24,13 @@ function drawGoogleVisualizations() {
     // Set a callback to run when the Google Visualization API is loaded.
     google.charts.setOnLoadCallback(drawCharts);
 }
+
+
 window.addEventListener('DOMContentLoaded', drawGoogleVisualizations);
 window.onresize = function(event) {
     drawGoogleVisualizations();
 };
+
+})(window.altana = window.altana || {});
+
+

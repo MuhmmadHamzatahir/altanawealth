b12e462a5382038344e3ba36e085280bd0f69e7a
Altana Wealth Prototype <altana@positivemojo.33mail.com>
2019-12-06 16:10:18 +0000

    Upload: assets/sign-up-popup.js

--- assets/sign-up-popup.js
+++ assets/sign-up-popup.js
@@ -1,16 +1,20 @@
-window.addEventListener('DOMContentLoaded', function() {
-    signUpCloseButtonClick();
-    document.getElementById('sign-up-close').addEventListener("click", signUpCloseButtonClick);
-    var signUpButton = document.getElementById('sign-up');
-    if (signUpButton != undefined) {signUpButton.addEventListener("click", signUpButtonClick)};
-});
+(function(altana, undefined) {
+    window.addEventListener('DOMContentLoaded', function() {
+        altana.signUpCloseButtonClick();
+        document.getElementById('sign-up-close').addEventListener("click", altana.signUpCloseButtonClick);
+        var signUpButton = document.getElementById('sign-up');
+        if (signUpButton != undefined) {
+            signUpButton.addEventListener("click", altana.signUpButtonClick)
+        };
+    });
 
-function signUpButtonClick(){
-    document.getElementById('sign-up-page').style.display = 'block'; // show
-    document.getElementById('cover').style.display = 'block'; 
-}
+    altana.signUpButtonClick = function() {
+        document.getElementById('sign-up-page').style.display = 'block'; // show
+        document.getElementById('cover').style.display = 'block';
+    }
 
-function signUpCloseButtonClick(){
-    document.getElementById('sign-up-page').style.display = 'none';
-    document.getElementById('cover').style.display = 'none'; 
-}
+    altana.signUpCloseButtonClick = function() {
+        document.getElementById('sign-up-page').style.display = 'none';
+        document.getElementById('cover').style.display = 'none';
+    }
+})(window.altana = window.altana || {});

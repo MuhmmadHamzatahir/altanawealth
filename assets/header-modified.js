82302026db0f4cd825cb247d8eba80deb980f81d
Altana Wealth Prototype <altana@positivemojo.33mail.com>
2019-12-06 15:37:56 +0000

    Upload: assets/header-modified.js

--- assets/header-modified.js
+++ assets/header-modified.js
@@ -1,26 +1,29 @@
-  var pagesModifiedHeader = [{
+(function(altana, undefined) {
+    var pagesModifiedHeader = [{
         pageName: "home"
-    },
-    {
+    }, {
         pageName: "home-new"
-    }
-];
-function addModifiedHeaderIfRequired() {
-    // get name of page
-    var thisPage = window.location.pathname.split('/').slice(-1)[0];
+    }];
 
-    // test if page exists in pagesModifiedHeader array
-// NOT WORK IN INTERNET EXPLORER    var result = pagesModifiedHeader.filter(x => x.pageName === thisPage).length;
-    var result = pagesModifiedHeader.filter(function(x) { return x.pageName === thisPage; }.length);
+    function addModifiedHeaderIfRequired() {
+        // get name of page
+        var thisPage = window.location.pathname.split('/').slice(-1)[0];
 
-    if (result != 0) {
-      document.getElementsByTagName("BODY")[0].classList.add("header-modified");
-    } else {
-        // alert("Page DOES NOT requires disclaimer to be accepted");
-    }
-};
-window.addEventListener('DOMContentLoaded', function() {
-//  addModifiedHeaderIfRequired()
-// No have on all pages
-      document.getElementsByTagName("BODY")[0].classList.add("header-modified");
-});
+        // test if page exists in pagesModifiedHeader array
+        // NOT WORK IN INTERNET EXPLORER    var result = pagesModifiedHeader.filter(x => x.pageName === thisPage).length;
+        var result = pagesModifiedHeader.filter(function(x) {
+            return x.pageName === thisPage;
+        }.length);
+
+        if (result != 0) {
+            document.getElementsByTagName("BODY")[0].classList.add("header-modified");
+        } else {
+            // alert("Page DOES NOT requires disclaimer to be accepted");
+        }
+    };
+    window.addEventListener('DOMContentLoaded', function() {
+        //  addModifiedHeaderIfRequired()
+        // No have on all pages
+        document.getElementsByTagName("BODY")[0].classList.add("header-modified");
+    });
+})(window.altana = window.altana || {});

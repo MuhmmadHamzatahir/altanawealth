249f700e416bc95a55ca8cc00d0a18d6b6ca13a8
Altana Wealth Prototype <altana@positivemojo.33mail.com>
2019-12-12 11:04:45 +0000

    Upload: assets/href-rebase.js

---
+++ assets/href-rebase.js
@@ -0,0 +1,20 @@
+(function(altana, undefined) {
+    function reBaseHrefs() {
+        // get list of href to files
+        var hrefs = Array.from(document.getElementsByTagName("A")).filter(linkOfInterest);
+
+        //disclaimer will log to console for now then open in blank window when implemented logic to go in disclaimerGate function
+        pdfTags.forEach(function(element) {
+//                element.href = href;
+            console.log(element.href);
+        });
+    };
+
+    function linkOfInterest(a) {
+        let soughtAddress = ["mybrightidea.squarespace.com"];
+        return soughtAddress.some(o => (a.href.toLowerCase()).includes(o));
+    }
+
+    window.addEventListener('DOMContentLoaded', reBaseHrefs);
+
+})(window.altana = window.altana || {});
